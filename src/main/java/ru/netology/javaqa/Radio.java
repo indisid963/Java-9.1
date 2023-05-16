f5821b2232b3adbd20d2db737ad4b7ea2d96de73
indi__sid <indisid.indisid@gmail.com>
2023-05-16 11:28:33 +0300

    Radio 2

--- src/main/java/ru/netology/javaqa/Radio.java
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -1,2 +1,72 @@
-package ru.netology.javaqa;public class Radio {
+package ru.netology.javaqa;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+
+    public void next() {
+        if (currentStation != 9) {
+            currentStation++;
+            return;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentStation != 0) {
+            currentStation--;
+            return;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public void increase() {
+        if (currentVolume != 100) {
+            currentVolume++;
+            return;
+        } else {
+            currentVolume = 100;
+        }
+    }
+
+    public void deicrease() {
+        if (currentVolume != 0) {
+            currentVolume--;
+            return;
+        } else {
+            currentVolume = 0;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+
+        }
+        if (currentVolume > 100) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,2 +1,131 @@
-package PACKAGE_NAME;public class RadioTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.javaqa.Radio;
+
+public class RadioTest {
+    @Test
+
+    public void TestNext() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(8);
+        radio.next();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestSetupNine() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.next();
+
+        int expected = 0;
+        int actuals = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+    @Test
+    public void TestPrev() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.prev();
+
+        int expected = 8;
+        int actuals = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+    @Test
+    public void TestPrevZero() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.prev();
+
+        int expected = 9;
+        int actuals = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+
+    @Test
+    public void TestIncreaceVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(2);
+        radio.increase();
+
+        int expected = 3;
+        int actuals = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+    @Test
+    public void TestDeincreaceVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(88);
+        radio.deicrease();
+
+        int expected = 87;
+        int actuals = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+    @Test
+    public void TestMoreMaxVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        radio.increase();
+
+        int expected = 100;
+        int actuals = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+    @Test
+    public void TestMinZero() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.deicrease();
+
+        int expected = 0;
+        int actuals = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+    @Test
+    public void TestExceedingStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(17);
+        int expected = 0;
+        int actuals = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+    @Test
+    public void TestExceedingVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(170);
+        int expected = 0;
+        int actuals = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+    @Test
+    public void TestLessStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(-7);
+        int expected = 0;
+        int actuals = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actuals);
+    }
+
+    @Test
+    public void TestLessVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(-100);
+        int expected = 0;
+        int actuals = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actuals);
+    }
 }
